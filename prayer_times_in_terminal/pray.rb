b02aff8936e4f3f5b4fc0f24bf440eeaf3fe79ab
elarabyelaidy <elarabyelaidy@gmail.com>
2024-03-03 13:19:47 +0200

    update prayer times

--- prayer_times_in_terminal/pray.rb
+++ prayer_times_in_terminal/pray.rb
@@ -3,17 +3,15 @@ require 'http'
 require 'json'
 require 'terminal-table'
 
-def get_prayer_times(city, country, year, month, method)
-  url = "https://api.aladhan.com/v1/calendarByCity/#{year}/#{month}"
-  headers = { 'Content-Type' => 'application/json' }
-  params = { city: city, country: country, method: method }
+def get_prayer_times(address, time)
+  url = "https://api.aladhan.com/v1/timingsByAddress/#{time}?address=#{address}"
 
   begin
-    response = HTTP.headers(headers).get(url, params: params)
+    response = HTTP.get(url)
     prayer_times_data = JSON.parse(response.body)
 
-    # puts prayer_times 
-    prayer_times = prayer_times_data['data'][0]['timings']
+    prayer_times = prayer_times_data['data']['timings']
+    prayer_times 
 
     { status: 'success', data: prayer_times }
   rescue StandardError => e
@@ -21,19 +19,17 @@ def get_prayer_times(city, country, year, month, method)
   end
 end
 
-city = 'Cairo'
-country = 'Egypt'
-year = Time.now.year.to_s
-month = Time.now.month.to_s
-method = '2'
+address = 'Mansourah'
+time = Time.now
+method = '5'
 
-result = get_prayer_times(city, country, year, month, method)
-# puts result
+result = get_prayer_times(address, time)
+ puts result
 if result[:status] == 'success'
-  rows = result[:data].map { |prayer, time| [prayer.capitalize, time] }.take(7)
+  rows = result[:data].map { |prayer, time| [prayer.capitalize, time] }
 
   table = Terminal::Table.new do |t|
-    t.title = "Prayer Times for #{city}, #{country} - #{year}/#{month}"
+    t.title = "Prayer Times for #{address} - #{time}"
     t.headings = ['Prayer', 'Time']
     t.rows = rows
   end
